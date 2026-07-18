7df3f9d8576e1ad7eb11f5dbc0f7f803c9c294c1
YTAL0 <ytalo122@hotmail.com>
2026-07-18 00:55:58 -0300

    Ufa

--- src/main/java/com/example/todo/Controller/TaskController.java
+++ src/main/java/com/example/todo/Controller/TaskController.java
@@ -4,7 +4,10 @@ import com.example.todo.Entity.Task;
 import com.example.todo.Service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -16,9 +19,17 @@ public class TaskController {
     public Task findByID(@PathVariable("id") Long id){
         return service.findByID(id);
     }
-
+    @GetMapping(value= "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Task create(@RequestBody Task task){
         return service.crete(task);
+    }
+    @DeleteMapping(value="/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/todo/Service/TaskService.java
+++ src/main/java/com/example/todo/Service/TaskService.java
@@ -2,9 +2,13 @@ package com.example.todo.Service;
 
 import com.example.todo.Entity.Task;
 import com.example.todo.Repository.TaskRepository;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Service
@@ -22,6 +26,12 @@ public class TaskService {
     public Task findByID(long id){
         return repository.findById(id).orElse(null);
     }
-
+    public List<Task> findAll(){
+        return repository.findAll();
+    }
+    public void delete(Long id){
+        Task task = repository.findById(id).orElseThrow(()->new RuntimeException("Not exist"));
+        repository.delete(task);
+    }
 
 }
