4a2bada8212de3ff1dd4a4fa80214339c6fd9706
YTAL0 <ytalo122@hotmail.com>
2026-07-17 23:11:02 -0300

    Uris

--- src/main/java/com/example/todo/Entity/Task.java
+++ src/main/java/com/example/todo/Entity/Task.java
@@ -1,4 +1,21 @@
 package com.example.todo.Entity;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "Tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ID;
+
+
 }
